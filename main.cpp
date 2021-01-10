305c9f005b599b82d89936a51e83af341d3b796d
CuzImAFK <67825866+CuzImAFK@users.noreply.github.com>
2021-01-10 13:35:42 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 
 int main()
 {
-    int c = 500;
-    bool warenviele = true; 
+    int c = 10;
+    bool warenviele = false; 
     warenviele = c > 100;
     if (warenviele) {
         std::string schlecht = "Ein massaker";
         std::cout << schlecht;
+    }
+   else {
+        std::string gut = "Ist es ja nochmal gut ausgegangen";
+        std::cout << gut;
+
     }
 
 
